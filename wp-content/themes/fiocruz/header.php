9ecabf4173ea7073f41348410297ac0ff504f09d
Maycon Duarte <mgam2004@gmail.com>
2022-10-28 17:28:58 -0300

    nav

--- wp-content/themes/fiocruz/header.php
+++ wp-content/themes/fiocruz/header.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<?php $viewport_content = apply_filters( 'hello_elementor_viewport_content', 'width=device-width, initial-scale=1' ); ?>
-	<meta name="viewport" content="<?php echo esc_attr( $viewport_content ); ?>">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <?php $viewport_content = apply_filters('hello_elementor_viewport_content', 'width=device-width, initial-scale=1'); ?>
+    <meta name="viewport" content="<?php echo esc_attr($viewport_content); ?>">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
 </head>
 <header>
-    
+    <nav class="nav-menu">
+        <?php
+        wp_nav_menu(
+            array(
+                'theme_location' => 'main_menu', // identificador do menu
+                'depth' => 1 // limita submenus
+            )
+        )
+        ?>
+    </nav>
 </header>
+
 <body <?php body_class(); ?>>
