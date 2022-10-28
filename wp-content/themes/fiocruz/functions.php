cacd36519006f686d9e8fcd75fd402df8293e325
Maycon Duarte <mgam2004@gmail.com>
2022-10-28 13:34:07 -0300

    nav header

--- wp-content/themes/fiocruz/functions.php
+++ wp-content/themes/fiocruz/functions.php
@@ -1,14 +1,17 @@
 <?php
 
-function theme_add_scripts() {
-    wp_enqueue_style( 'bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' );
-    wp_enqueue_style( 'style-css', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_script( 'bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js');
-    }
-    
-    add_action( 'wp_enqueue_scripts', 'theme_add_scripts');
-    
-function theme_add_config(){
+function theme_add_scripts()
+{
+    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css');
+    wp_enqueue_style('style-css', get_template_directory_uri() . '/style.css');
+    wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js');
+    wp_enqueue_script('vuejs', 'https://unpkg.com/vue@3/dist/vue.global.js');
+}
+
+add_action('wp_enqueue_scripts', 'theme_add_scripts');
+
+function theme_add_config()
+{
     register_nav_menus(
         array(
             'main_menu' => 'Main Menu'
--- wp-content/themes/fiocruz/header.php
+++ wp-content/themes/fiocruz/header.php
@@ -3,8 +3,22 @@
 
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php wp_title('|', true, 'right'); ?></title>
     <?php wp_head(); ?>
 </head>
+<header>
+    <nav class="nav-menu">
+        <?php
+        wp_nav_menu(
+            array(
+                'theme_location' => 'main_menu', // identificador do menu
+                'depth' => 1 // limita submenus
+            )
+        )
+        ?>
+    </nav>
+</header>
 
 <body <?php body_class(); ?>>
