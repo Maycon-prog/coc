280c5c1960ba555faba208a6339ea1c7f9986599
Maycon Duarte <mgam2004@gmail.com>
2022-10-28 10:38:28 -0300

    index

--- wp-content/themes/fiocruz/index.php
+++ wp-content/themes/fiocruz/index.php
@@ -1,1 +1,15 @@
-<?php get_header(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
+<?php wp_head(); ?>
+</head>
+<body>
+
+<?php the_content(); ?>
+
+<?php wp_footer(); ?>
+</body>
+</html>
