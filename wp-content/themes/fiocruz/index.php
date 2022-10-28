081d6bbab5fd8a9aa380592914d08a5585f1853c
Maycon Duarte <mgam2004@gmail.com>
2022-10-28 16:01:09 -0300

    teste

--- wp-content/themes/fiocruz/index.php
+++ wp-content/themes/fiocruz/index.php
@@ -1,5 +1,6 @@
 <?php get_header() ?>
     <? the_content() ?>
+    <iframe width="788" height="591" src="https://www.youtube.com/embed/ymspNPoaBIs" title="Why 4:3 Looks So Good" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <span>index.php</span>
 <?php get_footer(); ?>
 </body>
