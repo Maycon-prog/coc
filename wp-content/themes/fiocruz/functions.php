f94876e5b169868066a15ff168bbeba8dd846c99
Maycon Duarte <mgam2004@gmail.com>
2022-10-28 10:56:06 -0300

    add suport menus

--- wp-content/themes/fiocruz/functions.php
+++ wp-content/themes/fiocruz/functions.php
@@ -1,10 +1,18 @@
 <?php
 
-function theme_add_bootstrap() {
+function theme_add_scripts() {
     wp_enqueue_style( 'bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' );
     wp_enqueue_style( 'style-css', get_template_directory_uri() . '/style.css' );
     wp_enqueue_script( 'bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js');
     }
     
-    add_action( 'wp_enqueue_scripts', 'theme_add_bootstrap' );
-
+    add_action( 'wp_enqueue_scripts', 'theme_add_scripts');
+function theme_add_config(){
+    register_nav_menus(
+        array(
+            'main_menu' => 'Main Menu'
+            // 'identificador' => 'nome'
+        )
+    );
+}
+add_action('after_setup_theme', 'theme_add_config');
