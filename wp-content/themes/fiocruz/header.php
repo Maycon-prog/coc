c5219374264458f7682a2bcd499eacd7ef49f0f9
Maycon Duarte <mgam2004@gmail.com>
2022-10-28 17:32:18 -0300

    apaga tag header

--- wp-content/themes/fiocruz/header.php
+++ wp-content/themes/fiocruz/header.php
@@ -7,17 +7,5 @@
     <meta name="viewport" content="<?php echo esc_attr($viewport_content); ?>">
     <link rel="profile" href="https://gmpg.org/xfn/11">
 </head>
-<header>
-    <nav class="nav-menu">
-        <?php
-        wp_nav_menu(
-            array(
-                'theme_location' => 'main_menu', // identificador do menu
-                'depth' => 1 // limita submenus
-            )
-        )
-        ?>
-    </nav>
-</header>
 
 <body <?php body_class(); ?>>
