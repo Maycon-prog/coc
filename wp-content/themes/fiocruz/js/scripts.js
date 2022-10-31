3d742fb58d2f1a94d2ae8e567e37a43ae34e9c86
Maycon Duarte <mgam2004@gmail.com>
2022-10-31 12:22:22 -0300

    seta

--- wp-content/themes/fiocruz/js/scripts.js
+++ wp-content/themes/fiocruz/js/scripts.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     for (var i = 0; i < elms.length; i++) {
         new Splide(elms[i], {
-            arrowPath: 'M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z'
+            arrowPath: 'M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z'
         }).mount();
     }
 });
