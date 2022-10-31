c1fe1c27ab341a9e4edbee8fd5ed96c54a46ab88
Maycon Duarte <mgam2004@gmail.com>
2022-10-31 13:38:46 -0300

    up

--- wp-content/themes/fiocruz/custom-widgets/widget_slider.php
+++ wp-content/themes/fiocruz/custom-widgets/widget_slider.php
@@ -104,6 +104,7 @@ class widget_slider extends Widget_Base
                     </li>
                 </ul>
             </div>
+            <div class="splide__arrows"></div>
         </section> 
 <?php
     }
