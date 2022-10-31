4583134c22724f3c3e0f5bad90ad7c9a78ac5bbe
AdrielCavalcante <adriel.cavalcante2004@gmail.com>
2022-10-31 08:03:59 -0300

    Subindo widgets

---
+++ wp-content/themes/fiocruz/custom-widgets/widget1.php
@@ -0,0 +1,57 @@
+<?php
+namespace Elementor;
+
+class My_Widget_1 extends Widget_Base {
+	
+	public function get_name() {
+		return 'texto';
+	}
+	
+	public function get_title() {
+		return 'texto';
+	}
+	
+	public function get_icon() {
+		return 'fa fa-font';
+	}
+	
+	public function get_categories() {
+		return [ 'basic' ];
+	}
+	
+	protected function _register_controls() {
+
+		$this->start_controls_section(
+			'section_title',
+			[
+				'label' => __( 'Content', 'elementor' ),
+			]
+		);
+		
+		$this->add_control(
+			'Texto',
+			[
+				'label' => __( 'Texto', 'elementor' ),
+				'label_block' => true,
+				'type' => Controls_Manager::TEXTAREA,
+				'placeholder' => __( 'Insira um texto:', 'elementor' ),
+			]
+		);
+
+		$this->end_controls_section();
+	}
+	
+	protected function render() {
+
+        $settings = $this->get_settings_for_display();
+
+		echo  "<p lang="pt">$settings[Texto]</p>";
+		
+	}
+	
+	protected function _content_template() {
+
+    }
+	
+	
+}
--- wp-content/themes/fiocruz/functions.php
+++ wp-content/themes/fiocruz/functions.php
@@ -20,4 +20,5 @@ function theme_add_config()
     );
 }
 add_action('after_setup_theme', 'theme_add_config', 0);
-//require_once('widgets/my-widgets.php');
+
+require_once("custom-widgets/my-widgets.php");
