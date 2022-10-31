c1d5ec19ad3996257142081dd26272446d7c5e0a
Maycon Duarte <mgam2004@gmail.com>
2022-10-31 13:37:17 -0300

    up

--- wp-content/themes/fiocruz/custom-widgets/widget_slider.php
+++ wp-content/themes/fiocruz/custom-widgets/widget_slider.php
@@ -96,19 +96,11 @@ class widget_slider extends Widget_Base
                 <ul class="splide__list">
                     <li class="splide__slide">
                         <h2 class="slide-title">Titulo do Slide 1</h2>
-                        <p class="slide-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        <p class="slide-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
                     </li>
                     <li class="splide__slide">
-                        <h2 class="slide-title">Titulo do Slide 2</h2>
-                        <p class="slide-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                    </li>
-                    <li class="splide__slide">
-                        <h2 class="slide-title">Titulo do Slide 3</h2>
-                        <p class="slide-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                    </li>
-                    <li class="splide__slide">
-                        <h2 class="slide-title">Titulo do Slide 4</h2>
-                        <p class="slide-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        <h2 class="slide-title">Titulo do Slide 1</h2>
+                        <p class="slide-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
                     </li>
                 </ul>
             </div>
