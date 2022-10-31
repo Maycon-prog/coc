5846f8826f60609f11e780c295c61fb58eff8af4
Maycon Duarte <mgam2004@gmail.com>
2022-10-31 11:35:43 -0300

    resolve

--- wp-content/themes/fiocruz/js/scripts.js
+++ wp-content/themes/fiocruz/js/scripts.js
@@ -1,8 +1,3 @@
 
-var widgets_slides = document.getElementsByClassName('splide');
-for (let i = 0; i < widgets_slides.length; i++) {
-    new Splide(widgets_slides[i]).mount();
-    console.log('slider' + i);
-}
-console.log('scripts.js');
+new Splide('.slide').mount();
 
