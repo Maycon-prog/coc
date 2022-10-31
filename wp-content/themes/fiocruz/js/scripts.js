6bd975bcd500ca48dbad9c674b7c4ea09bafefe7
Maycon Duarte <mgam2004@gmail.com>
2022-10-31 11:29:34 -0300

    splider

--- wp-content/themes/fiocruz/js/scripts.js
+++ wp-content/themes/fiocruz/js/scripts.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var widgets_slides = document.getElementsByClassName('.splide');
     for (let i = 0; i < widgets_slides.length; i++) {
         new Splide(widgets_slides[i]).mount();
+        console.log('slider'+i);
     }
 });
 
