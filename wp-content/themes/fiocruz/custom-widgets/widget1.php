ec3d3d6b2e3909c05923562d1ecb3fccb368f60b
AdrielCavalcante <adriel.cavalcante2004@gmail.com>
2022-10-31 17:15:19 -0300

    Testando wrapper

--- wp-content/themes/fiocruz/custom-widgets/widget1.php
+++ wp-content/themes/fiocruz/custom-widgets/widget1.php
@@ -33,8 +33,38 @@ class My_Widget_1 extends Widget_Base {
 			[
 				'label' => __( 'Texto', 'elementor' ),
 				'label_block' => true,
-				'type' => Controls_Manager::WYSIWYG,
+				'type' => Controls_Manager::TEXTAREA,
 				'placeholder' => __( 'Insira um texto:', 'elementor' ),
+			]
+		);
+
+		$this->start_controls_section(
+			'section_title',
+			[
+				'label' => __( 'Style', 'elementor' ),
+			]
+		);
+		
+		$this->add_control(
+			'Cor',
+			[
+				'label' => __( 'Cor', 'elementor' ),
+				'label_block' => true,
+				'type' => Controls_Manager::COLOR,
+				'default' => '#000',
+				'placeholder' => __( 'Insira cor para um texto:', 'elementor' ),
+			]
+		);
+
+		$this->add_control(
+			'Fonte',
+			[
+				'label' => __( 'Fonte', 'elementor' ),
+				'label_block' => true,
+				'type' => Controls_Manager::FONT,
+				'selectors' => [
+					'{{WRAPPER}} p' => 'text-align: {{VALUE}}',
+				],
 			]
 		);
 
@@ -45,7 +75,11 @@ class My_Widget_1 extends Widget_Base {
 
         $settings = $this->get_settings_for_display();
 
-		echo $settings['Texto'];
+		?>
+
+		<p lang="pt" style="color: <?php echo $settings['Cor']; ?>; "><?php echo $settings['Texto']; ?></p>
+
+		<?php
 		
 	}
 	
