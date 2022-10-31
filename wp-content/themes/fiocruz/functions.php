46634b82225fd6e0771b87724db46aea7f71f106
Maycon Duarte <mgam2004@gmail.com>
2022-10-31 11:16:36 -0300

    rename

--- wp-content/themes/fiocruz/functions.php
+++ wp-content/themes/fiocruz/functions.php
@@ -8,7 +8,7 @@ function theme_add_scripts()
     wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js');
     wp_enqueue_script('vuejs', 'https://unpkg.com/vue@3/dist/vue.global.js');
     wp_enqueue_script('splider-js', 'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.3/dist/js/splide.min.js');
-    wp_enqueue_style('splider-css', 'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.3/dist/js/splide.min.css');
+    wp_enqueue_style('splider-css', 'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.3/dist/css/splide.min.css');
 }
 
 add_action('wp_enqueue_scripts', 'theme_add_scripts');
