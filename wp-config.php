046fdffff0f6c31303f413b98783b8cabfff0f77
AdrielCavalcante <adriel.cavalcante2004@gmail.com>
2022-10-31 09:19:47 -0300

    teste

--- wp-config.php
+++ wp-config.php
@@ -82,10 +82,8 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
-define( ‘WP_DEBUG_DISPLAY’, false );
-@ini_set( ‘display_errors’, 0 );
-define( ‘SCRIPT_DEBUG’, false );
+define( 'WP_DEBUG', true );
+
 
 /* Adicione valores personalizados entre esta linha até "Isto é tudo". */
 
