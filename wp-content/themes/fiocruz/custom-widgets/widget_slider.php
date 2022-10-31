dab3de00ead3e1a0eef7c8e006c444fed42e23b1
Maycon Duarte <mgam2004@gmail.com>
2022-10-31 11:22:07 -0300

    splider

--- wp-content/themes/fiocruz/custom-widgets/widget_slider.php
+++ wp-content/themes/fiocruz/custom-widgets/widget_slider.php
@@ -107,7 +107,7 @@ class widget_slider extends Widget_Base
             </div>
         </section>
         <script>
-            new Splide('#slider').mount();
+            
         </script>
 <?php
     }
