023473801438cace376f6ac0fe54492bded7b20f
Maycon Duarte <mgam2004@gmail.com>
2022-10-31 12:21:35 -0300

    arrow

--- wp-content/themes/fiocruz/js/scripts.js
+++ wp-content/themes/fiocruz/js/scripts.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', function () {
     var elms = document.getElementsByClassName('splide');
 
     for (var i = 0; i < elms.length; i++) {
-        new Splide(elms[i]).mount();
+        new Splide(elms[i], {
+            arrowPath: 'M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z'
+        }).mount();
     }
 });
 
