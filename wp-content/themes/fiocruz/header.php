b7e948c7699c582b3556e14a7f6504a6adaefd9a
Maycon Duarte <mgam2004@gmail.com>
2022-10-31 15:27:16 -0300

    teste

--- wp-content/themes/fiocruz/header.php
+++ wp-content/themes/fiocruz/header.php
@@ -6,7 +6,7 @@
     <?php $viewport_content = apply_filters('hello_elementor_viewport_content', 'width=device-width, initial-scale=1'); ?>
     <meta name="viewport" content="<?php echo esc_attr($viewport_content); ?>">
     <link rel="profile" href="https://gmpg.org/xfn/11">
-    <title><?php wp_title('-'); ?> teste</title>
+    <title><?php wp_title('-', true); ?> teste</title>
     <?php wp_head(); ?>
 </head>
 <header>
