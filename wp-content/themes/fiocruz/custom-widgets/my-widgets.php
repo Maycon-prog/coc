adad0a1ad04589a56991ca8358d534a02a80b4a7
Maycon Duarte <mgam2004@gmail.com>
2022-11-01 11:33:40 -0300

    up

--- wp-content/themes/fiocruz/custom-widgets/my-widgets.php
+++ wp-content/themes/fiocruz/custom-widgets/my-widgets.php
@@ -1,35 +1,44 @@
 <?php
 
-class My_Elementor_Widgets {
+class My_Elementor_Widgets
+{
 
 	protected static $instance = null;
 
-	public static function get_instance() {
-		if ( ! isset( static::$instance ) ) {
+	public static function get_instance()
+	{
+		if (!isset(static::$instance)) {
 			static::$instance = new static;
 		}
 
 		return static::$instance;
 	}
 
-	protected function __construct() { 
+	protected function __construct()
+	{
+
+		foreach (glob(get_template_directory() . "custom-widgets/widgets/*.php") as $filename) {
+			require_once($filename);
+		}
+
 		require_once('widget1.php');
 		require_once('widget2.php');
 		require_once('widget3.php');
 		require_once('widget_slider.php');
-		add_action( 'elementor/widgets/widgets_registered', [ $this, 'register_widgets' ] );
+		add_action('elementor/widgets/widgets_registered', [$this, 'register_widgets']);
 	}
 
-	public function register_widgets() {
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Elementor\My_Widget_1() );
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Elementor\My_Widget_2() );
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Elementor\My_Widget_3() );
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Elementor\widget_slider() );
+	public function register_widgets()
+	{
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new \Elementor\My_Widget_1());
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new \Elementor\My_Widget_2());
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new \Elementor\My_Widget_3());
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new \Elementor\widget_slider());
 	}
-
 }
 
-add_action( 'init', 'my_elementor_init' );
-function my_elementor_init() {
+add_action('init', 'my_elementor_init');
+function my_elementor_init()
+{
 	My_Elementor_Widgets::get_instance();
 }
