0faf2666a6f15e4de2916667ff96f3371d50a895
Maycon Duarte <mgam2004@gmail.com>
2022-11-01 11:28:32 -0300

    up

--- wp-content/themes/fiocruz/page-home.php
+++ wp-content/themes/fiocruz/page-home.php
@@ -2,20 +2,4 @@
 
 <?php the_content(); ?>
 
-<?php
-$pasta = get_template_directory() . '/custom-widgets';
-
-if (is_dir($pasta)) {
-    $diretorio = dir($pasta);
-
-    while (($arquivo = $diretorio->read()) !== false) {
-        echo '<a href=' . $pasta . $arquivo . '>' . $arquivo . '</a><br />';
-    }
-
-    $diretorio->close();
-} else {
-    echo 'A pasta não existe.';
-}
-?>
-
 <?php get_footer(); ?>
