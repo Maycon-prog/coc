1ee6d39233e38d697c498362ad1c34f778e4e094
AdrielCavalcante <adriel.cavalcante2004@gmail.com>
2022-11-01 16:13:02 -0300

    Subindo permanencia de fonte ao mudar de pag

--- wp-content/themes/fiocruz/js/scripts.js
+++ wp-content/themes/fiocruz/js/scripts.js
@@ -15,12 +15,24 @@ document.addEventListener('DOMContentLoaded', function () {
     if(sessionStorage.getItem('tema') == 'escuro') {
         document.getElementById('pagina').classList.add("dark-mode");
     }
+
+    if(sessionStorage.getItem('fonte') == 'aumentada') {
+        document.getElementById('pagina').classList.add("aumentou");
+        document.getElementById('pagina').classList.remove("diminuiu");
+    } else if(sessionStorage.getItem('fonte') == 'diminuida') {
+        document.getElementById('pagina').classList.add("diminuiu");
+        document.getElementById('pagina').classList.remove("aumentou");
+    } else {
+        document.getElementById('pagina').classList.remove("aumentou");
+        document.getElementById('pagina').classList.remove("diminuiu");
+    }
 });
 
 function contraste() {   
     if(document.getElementById('pagina').className.includes("dark-mode")){
         document.getElementById('pagina').classList.remove("dark-mode");
         document.getElementById('pagina').classList.add("light-mode");
+        sessionStorage.removeItem('tema');
     } else {    
         document.getElementById('pagina').classList.add("dark-mode");
         document.getElementById('pagina').classList.remove("light-mode");
@@ -32,12 +44,17 @@ function tamanhoFont(tamanho) {
     if(tamanho == 'normal') {
         document.getElementById('pagina').classList.remove("diminuiu");
         document.getElementById('pagina').classList.remove("aumentou");
+        sessionStorage.removeItem('fonte');
+        
     } else if(tamanho == 'sub') {
         document.getElementById('pagina').classList.add("diminuiu");
         document.getElementById('pagina').classList.remove("aumentou");
+        sessionStorage.setItem('fonte', 'aumentada');
+        
     } else {
         document.getElementById('pagina').classList.add("aumentou");
         document.getElementById('pagina').classList.remove("diminuiu");
+        sessionStorage.setItem('fonte', 'diminuida');
     }
 }
 
