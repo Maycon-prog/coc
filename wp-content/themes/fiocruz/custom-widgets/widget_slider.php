8a1bb7fcbe820fd119ea3ead30c1b6ee2e844350
Maycon Duarte <mgam2004@gmail.com>
2022-11-01 10:44:38 -0300

    up

--- wp-content/themes/fiocruz/custom-widgets/widget_slider.php
+++ wp-content/themes/fiocruz/custom-widgets/widget_slider.php
@@ -90,7 +90,7 @@ class widget_slider extends Widget_Base
 
         $settings = $this->get_settings_for_display();
 ?>
-        <section id="splide" class="splide" aria-label="Beautiful Images">
+        <section id="splide" data-splide='{"type":"loop","perPage":1}' class="splide">
             <div class="splide__track">
                 <ul class="splide__list">
                     <?php
