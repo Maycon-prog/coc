c6dd22d8513fab276acbcd541b514fdf5cf208d1
Adriel Cavalcante <adriel.cavalcante2004@gmail.com>
2022-11-03 17:30:33 -0300

    teste

--- wp-content/themes/fiocruz/header.php
+++ wp-content/themes/fiocruz/header.php
@@ -49,7 +49,7 @@
                             <div class="bar3"></div>
                         </div>
                     </button>
-                    <h2 class="legenda-header">Programa de Pós-Graduação em Divulgação da Ciência, Tecnologia e Saúde</h2>
+                    <h1 class="legenda-header">Programa de Pós-Graduação em Divulgação da Ciência, Tecnologia e Saúde</h1>
                 </div>
             </div>
             <nav class="nav-menu">
@@ -69,7 +69,7 @@
 
             <div id="menu-mobile">
                 <button onclick="button_menu('close');" class="close-button"><i class="bi bi-x-circle"></i></button>
-                <h1 class="legenda-mobile">Programa de Pós-Graduação em Divulgação da Ciência, Tecnologia e Saúde</h1>
+                <h2 class="legenda-mobile">Programa de Pós-Graduação em Divulgação da Ciência, Tecnologia e Saúde</h2>
                 <div class="menu-header-mobile">
                     <nav class="nav-menu-mobile">
                         <?php
