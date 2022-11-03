3d5a2d9c72d925a03ad7e37f57794b03367f9f7d
Maycon Duarte <mgam2004@gmail.com>
2022-11-03 15:26:16 -0300

    teste

--- wp-content/themes/fiocruz/custom-widgets/widgets/widget_relacionado.php
+++ wp-content/themes/fiocruz/custom-widgets/widgets/widget_relacionado.php
@@ -28,48 +28,19 @@ class widget_relacionado extends Widget_Base
     protected function _register_controls()
     {
 
-        $this->start_controls_section(
-            'content_section',
-            [
-                'label' => esc_html__('Content', 'elementor'),
-                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
-            ]
-        );
-
-        $this->add_control(
-            'link',
-            [
-                'label' => esc_html__('Link', 'textdomain'),
-                'type' => \Elementor\Controls_Manager::URL,
-                'placeholder' => esc_html__('https://your-link.com', 'textdomain'),
-                'options' => ['url', 'is_external', 'nofollow'],
-                'default' => [
-                    'url' => '',
-                    'is_external' => true,
-                    'nofollow' => true,
-                    // 'custom_attributes' => '',
-                ],
-                'label_block' => true,
-            ]
-        );
-
-        $this->end_controls_section();
     }
 
     protected function render()
     {
-        for ($i = 1; $i < 5; $i++) {
-            if (isset(get_field("post_relacionado$i")->ID)) {
-                $post_id = get_field("post_relacionado$i")->ID;
-                $titulo = get_the_title($post_id);
-                $thumbnail = get_the_post_thumbnail_url($post_id);
+        if (isset(get_field("post_relacionado1")->ID)) {
+            $post_id = get_field("post_relacionado$i")->ID;
+            $titulo = get_the_title($post_id);
+            $thumbnail = get_the_post_thumbnail_url($post_id);
 ?>
-                <div class="img-relacionado" style="background-image: url('<?php echo $thumbnail ?>')"></div>
-                <h2 class="titulo-relacionado"><?php echo $titulo ?></h2>
+            <div class="img-relacionado" style="background-image: url('<?php echo $thumbnail ?>')"></div>
+            <h2 class="titulo-relacionado"><?php echo $titulo ?></h2>
         <?php
-            }
         }
-
         ?>
 
 <?php
