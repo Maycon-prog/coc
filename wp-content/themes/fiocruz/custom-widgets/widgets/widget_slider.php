0205d90b49a2cc7dd7532cc4198b5963eb737f7e
Maycon Duarte <mgam2004@gmail.com>
2022-11-03 11:09:15 -0300

    teste

--- wp-content/themes/fiocruz/custom-widgets/widgets/widget_slider.php
+++ wp-content/themes/fiocruz/custom-widgets/widgets/widget_slider.php
@@ -111,7 +111,7 @@ class widget_slider extends Widget_Base
 
         $settings = $this->get_settings_for_display();
 ?>      
-        <section style="background-color: <?php echo $settings['marker_color'] ?>" id="splide" data-splide='{"type":"loop","perPage":1}' class="splide">
+        <section id="splide" data-splide='{"type":"loop","perPage":1}' class="splide">
             <div class="splide__track">
                 <ul class="splide__list">
                     <?php
