5568cd77b1c12345112c428dd4556a31716facd3
Maycon Duarte <mgam2004@gmail.com>
2022-11-03 14:11:29 -0300

    new widget

---
+++ wp-content/themes/fiocruz/custom-widgets/widgets/widget_relacionado.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Elementor;
+
+class widget_relacionado extends Widget_Base
+{
+
+    public function get_name()
+    {
+        return 'relacionado'; // nome para usar no código
+    }
+
+    public function get_title()
+    {
+        return 'Relacionado'; // nome para o usuario
+    }
+
+    public function get_icon()
+    {
+        return 'eicon-editor-external-link'; // https://elementor.github.io/elementor-icons/
+    }
+
+    public function get_categories()
+    {
+        return ['widgets-personalizados'];
+    }
+
+    protected function _register_controls()
+    {
+
+        $this->start_controls_section(
+            'content_section',
+            [
+                'label' => esc_html__('Content', 'elementor'),
+                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+
+        $this->add_control(
+            'link',
+            [
+                'label' => esc_html__('Link', 'textdomain'),
+                'type' => \Elementor\Controls_Manager::URL,
+                'placeholder' => esc_html__('https://your-link.com', 'textdomain'),
+                'options' => ['url', 'is_external', 'nofollow'],
+                'default' => [
+                    'url' => '',
+                    'is_external' => true,
+                    'nofollow' => true,
+                    // 'custom_attributes' => '',
+                ],
+                'label_block' => true,
+            ]
+        );
+
+        $this->end_controls_section();
+    }
+
+    protected function render()
+    {
+
+        $settings = $this->get_settings_for_display();
+        print_r($settings);
+?>
+
+<?php
+    }
+
+    protected function _content_template()
+    {
+    }
+}
