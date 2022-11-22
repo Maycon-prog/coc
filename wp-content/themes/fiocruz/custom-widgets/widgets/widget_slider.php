26abc304bad3fa827c432362b250b8d0b0cacb31
AdrielCavalcante <adriel.cavalcante2004@gmail.com>
2022-11-22 14:03:22 -0300

    Adicionando tab index no content slide

--- wp-content/themes/fiocruz/custom-widgets/widgets/widget_slider.php
+++ wp-content/themes/fiocruz/custom-widgets/widgets/widget_slider.php
@@ -97,8 +97,8 @@ class widget_slider extends Widget_Base
                         for ($i = 0; $i < count($settings['list']); $i++) {
                         ?>
                             <li class="splide__slide">
-                                <h2 class="slide-title texto"><?= $settings['list'][$i]['list_title'] ?></h2>
-                                <p class="slide-content texto"><?= $settings['list'][$i]['list_content'] ?></p>
+                                <h2 tabindex="0" class="slide-title texto"><?= $settings['list'][$i]['list_title'] ?></h2>
+                                <p tabindex="0" class="slide-content texto"><?= $settings['list'][$i]['list_content'] ?></p>
                             </li>
                         <?php
                         }
