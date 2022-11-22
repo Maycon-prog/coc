154a90fa01264f073e66f0b655a41ae2b4777353
Maycon Duarte <mgam2004@gmail.com>
2022-11-22 15:45:00 -0300

    remove script

--- wp-content/themes/fiocruz/header.php
+++ wp-content/themes/fiocruz/header.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
     <?php wp_head() ?>
-    <script src="js/scripts.js"></script>
 </head>
 
 <body id="body" <?php body_class(); ?>>
