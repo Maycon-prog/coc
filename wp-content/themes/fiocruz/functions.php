ee2c5f1be3619b29d2492374e38c19ef160e80c3
Maycon Duarte <mgam2004@gmail.com>
2022-11-23 17:12:21 -0300

    teste

--- wp-content/themes/fiocruz/functions.php
+++ wp-content/themes/fiocruz/functions.php
@@ -2,16 +2,14 @@
 
 function theme_add_scripts()
 {
-    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css');
+    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css');
     wp_enqueue_style('bootstrap-icons','https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css'); 
     wp_enqueue_style('style-css', get_template_directory_uri() . '/style.css');
     wp_enqueue_script('scripts-js', get_template_directory_uri() . '/js/scripts.js');
     wp_enqueue_script('jquery', 'https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js');
     wp_enqueue_script('bootstrap-popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js');
-    wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2-beta1/dist/js/bootstrap.bundle.min.js');
+    wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js');
     wp_enqueue_script('vuejs', 'https://unpkg.com/vue@3/dist/vue.global.js');
-    wp_enqueue_script('splider-js', 'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.3/dist/js/splide.min.js');
-    wp_enqueue_style('splider-css', 'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.3/dist/css/splide.min.css');
 }
 
 add_action('wp_enqueue_scripts', 'theme_add_scripts');
