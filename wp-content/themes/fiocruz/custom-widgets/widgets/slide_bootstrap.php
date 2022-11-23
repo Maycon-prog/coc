db6c8e12479d833c6966bd7ea4724687df9c219f
Maycon Duarte <mgam2004@gmail.com>
2022-11-23 17:51:47 -0300

    teste

--- wp-content/themes/fiocruz/custom-widgets/widgets/slide_bootstrap.php
+++ wp-content/themes/fiocruz/custom-widgets/widgets/slide_bootstrap.php
@@ -91,28 +91,28 @@ class widget_slider_bootstrap extends Widget_Base
         $settings = $this->get_settings_for_display();
         $id = uniqid("slider_");
 ?>
-        <div id="<?=$id?>" class="carousel slide" data-bs-ride="carousel" data-bs-interval="false" data-bs-wrap="false">
+        <div id="<?= $id ?>" class="carousel slide" data-bs-ride="carousel" data-bs-interval="false" data-bs-wrap="false">
             <ol class="carousel-indicators">
-                <li data-target="#<?=$id?>" data-slide-to="0" class="active"></li>
-                <li data-target="#<?=$id?>" data-slide-to="1"></li>
-                <li data-target="#<?=$id?>" data-slide-to="2"></li>
+                <li data-target="#<?= $id ?>" data-slide-to="0" class="active"></li>
+                <li data-target="#<?= $id ?>" data-slide-to="1"></li>
+                <li data-target="#<?= $id ?>" data-slide-to="2"></li>
             </ol>
             <div class="carousel-inner">
-                <div class="carousel-item active" tabindex="0">
-                    <h2>Teste de leitura 1</h2>
-                </div>
-                <div class="carousel-item" tabindex="0">
-                    <h2>Teste de leitura 2</h2>
-                </div>
-                <div class="carousel-item" tabindex="0">
-                    <h2>Teste de leitura 3</h2>
-                </div>
+                <?php
+                for ($i = 0; $i < count($settings['list']); $i++) {
+                ?>
+                    <div class="carousel-item <?php $i == 1 ? 'active' : ''; ?>" tabindex="0">
+                        <h2>Teste de leitura 1</h2>
+                    </div>
+                <?php
+                }
+                ?>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="<?='#'.$id?>" data-bs-slide="prev">
+            <button class="carousel-control-prev" type="button" data-bs-target="<?= '#' . $id ?>" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="<?='#'.$id?>" data-bs-slide="next">
+            <button class="carousel-control-next" type="button" data-bs-target="<?= '#' . $id ?>" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
