bf5efda6e45dbf6c47ede073105a3792d9e8005f
Maycon Duarte <mgam2004@gmail.com>
2022-11-23 17:36:26 -0300

    teste

--- wp-content/themes/fiocruz/custom-widgets/widgets/slide_bootstrap.php
+++ wp-content/themes/fiocruz/custom-widgets/widgets/slide_bootstrap.php
@@ -98,6 +98,7 @@ class widget_slider_bootstrap extends Widget_Base
                 <li data-target="#<?= $id ?>" data-slide-to="2"></li>
             </ol>
             <div class="carousel-inner">
+                
                 <div class="carousel-item active">
                     <div class="conteudo-carousel"  tabindex="0">
                         <h2>Teste de leitura 1</h2>
