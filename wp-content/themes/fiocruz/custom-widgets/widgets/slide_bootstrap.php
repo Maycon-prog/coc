daf49af4308b791af7fe6f241d60febb8ff39ac4
Maycon Duarte <mgam2004@gmail.com>
2022-11-23 16:56:03 -0300

    teste

--- wp-content/themes/fiocruz/custom-widgets/widgets/slide_bootstrap.php
+++ wp-content/themes/fiocruz/custom-widgets/widgets/slide_bootstrap.php
@@ -91,11 +91,11 @@ class widget_slider_bootstrap extends Widget_Base
         $settings = $this->get_settings_for_display();
         $id = uniqid("slider_");
 ?>
-        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div id="<?=$id?>" class="carousel slide" data-bs-ride="carousel">
             <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#<?=$id?>" data-slide-to="0" class="active"></li>
+                <li data-target="#<?=$id?>" data-slide-to="1"></li>
+                <li data-target="#<?=$id?>" data-slide-to="2"></li>
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active" tabindex="0">
@@ -108,11 +108,11 @@ class widget_slider_bootstrap extends Widget_Base
                     <h2>Teste de leitura 3</h2>
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <button class="carousel-control-prev" type="button" data-bs-target="#<?=$id?>" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <button class="carousel-control-next" type="button" data-bs-target="#<?=$id?>" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
