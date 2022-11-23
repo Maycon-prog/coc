0aa7eeae11326a5d4d44017a0dfc9cf03d12d519
Maycon Duarte <mgam2004@gmail.com>
2022-11-23 15:59:39 -0300

    teste

--- wp-content/themes/fiocruz/page-home.php
+++ wp-content/themes/fiocruz/page-home.php
@@ -1,6 +1,7 @@
 <?php get_header(); 
 		$short_dir = './custom-widgets/widgets';
         echo $short_dir;
+        var_dump(scandir($short_dir));
         $myfiles = array_diff(scandir($short_dir), array('.', '..'));
 
         foreach ($myfiles as $file) {
