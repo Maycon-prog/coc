ec539946b68b25eb517348e62df1a0c6de3e226f
Maycon Duarte <mgam2004@gmail.com>
2022-11-23 16:07:19 -0300

    teste

--- wp-content/themes/fiocruz/custom-widgets/my-widgets.php
+++ wp-content/themes/fiocruz/custom-widgets/my-widgets.php
@@ -1,44 +1,49 @@
 <?php
 
-class My_Elementor_Widgets {
+class My_Elementor_Widgets
+{
 
 	protected static $instance = null;
 
-	public static function get_instance() {
-		if ( ! isset( static::$instance ) ) {
+	public static function get_instance()
+	{
+		if (!isset(static::$instance)) {
 			static::$instance = new static;
 		}
 
 		return static::$instance;
 	}
 
-	protected function __construct() { 
+	protected function __construct()
+	{
+		$short_dir = get_stylesheet_directory() . '/custom-widgets/widgets';
+		$myfiles = array_diff(scandir($short_dir), array('.', '..'));
+		foreach ($myfiles as $file) {
+			require_once('widgets/'.$file);
 
-
-		require_once('widgets/widget1.php');
-		require_once('widgets/widget2.php');
-		require_once('widgets/widget3.php');
-		require_once('widgets/title.php');
-		require_once('widgets/img.php');
-		require_once('widgets/widget_slider.php');
-		require_once('widgets/widget_relacionado.php');
-		add_action( 'elementor/widgets/widgets_registered', [ $this, 'register_widgets' ] );
-	} 
- 
-	public function register_widgets() {
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Elementor\My_Widget_1() );
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Elementor\My_Widget_2() );
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Elementor\My_Widget_3() );
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Elementor\title());
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Elementor\imagem());
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Elementor\widget_slider() );
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Elementor\widget_relacionado() );
-
+		}
+		add_action('elementor/widgets/widgets_registered', [$this, 'register_widgets']);
 	}
 
+	public function register_widgets()
+	{
+		foreach ($myfiles as $file) {
+			require_once('widgets/'.$file);
+			$name = str_replace(['.php'], '', $file);
+		}
+		
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new \Elementor\My_Widget_1());
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new \Elementor\My_Widget_2());
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new \Elementor\My_Widget_3());
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new \Elementor\title());
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new \Elementor\imagem());
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new \Elementor\widget_slider());
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new \Elementor\widget_relacionado());
+	}
 }
 
-add_action( 'init', 'my_elementor_init' );
-function my_elementor_init() {
+add_action('init', 'my_elementor_init');
+function my_elementor_init()
+{
 	My_Elementor_Widgets::get_instance();
 }
--- wp-content/themes/fiocruz/page-home.php
+++ wp-content/themes/fiocruz/page-home.php
@@ -1,10 +1,4 @@
-<?php get_header(); 
-
-$short_dir = get_stylesheet_directory() . '/custom-widgets/widgets';
-$myfiles = array_diff(scandir($short_dir), array('.', '..'));
-var_dump($myfiles);
-
-?>
+<?php get_header(); ?>
 
 <main>
 <?php the_content(); ?>
